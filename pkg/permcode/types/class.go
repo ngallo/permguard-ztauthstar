74d55d28d8647f3916b3922a88ce66884d94a324
Nicola Gallo <nicolagmt@hotmail.com>
2024-12-03 15:28:47 +0100

    refactor: rethinking the permcode layer

--- pkg/permcode/types/class.go
+++ pkg/permcode/types/class.go
@@ -16,74 +16,17 @@
 
 package types
 
-import (
-	"fmt"
-)
-
 const (
-	// PermCodeLanguage is the permcode language.
-	PermCodeLanguage = "permcode"
-	// PermCodeLanguageID is the permcode language ID.
-	PermCodeLanguageID = uint32(1)
-
-	// PermCodeSyntaxLatest is the latest permcode syntax.
-	PermCodeSyntaxLatest = "permcode1"
-	// PermCodeSyntaxIDLatest is the latest permcode syntax ID.
-	PermCodeSyntaxIDLatest = uint32(1)
-
 	// ClassTypeSchema is the object type for domain schemas.
 	ClassTypeSchema = "schema"
 	// ClassTypeObject is the object type for domain objects.
 	ClassTypeIDSchema = uint32(1)
 
-	// ClassTypeACPermission is the class type for an access control permission.
-	ClassTypeACPermission = "acpermission"
-	// ClassTypeIDACPermission is the object type for an access control permission.
-	ClassTypeIDACPermission = uint32(2)
-
 	// ClassTypeACPolicy is the object type for an access control policy.
 	ClassTypeACPolicy = "acpolicy"
 	// ClassTypeIDACPolicy is the object type for an access control policy.
-	ClassTypeIDACPolicy = uint32(3)
+	ClassTypeIDACPolicy = uint32(1)
 )
-
-// GetClassTypeID returns the language id, syntax id, and class type id.
-func GetClassTypeID(classType string) (uint32, uint32,  uint32, error) {
-	var classTypeID uint32
-	switch classType {
-	case ClassTypeSchema:
-		classTypeID = ClassTypeIDSchema
-	case ClassTypeACPermission:
-		classTypeID = ClassTypeIDACPermission
-	case ClassTypeACPolicy:
-		classTypeID = ClassTypeIDACPolicy
-	default:
-		return 0, 0, 0, fmt.Errorf("permcode: invalid class type %s", classType)
-	}
-	return PermCodeLanguageID, PermCodeSyntaxIDLatest, classTypeID, nil
-}
-
-// GetClassType returns the language, syntax, and class type.
-func GetClassType(langID, syntaxID, classTypeID uint32) (string, string,  string, error) {
-	if langID != PermCodeLanguageID {
-		return "", "", "", fmt.Errorf("permcode: invalid language ID %d", langID)
-	}
-	if syntaxID != PermCodeSyntaxIDLatest {
-		return "", "", "", fmt.Errorf("permcode: invalid syntax ID %d", syntaxID)
-	}
-	var classType string
-	switch classTypeID {
-	case ClassTypeIDSchema:
-		classType = ClassTypeSchema
-	case ClassTypeIDACPermission:
-		classType = ClassTypeACPermission
-	case ClassTypeIDACPolicy:
-		classType = ClassTypeACPolicy
-	default:
-		return "", "", "", fmt.Errorf("permcode: invalid class type %d", classTypeID)
-	}
-	return PermCodeLanguage, PermCodeSyntaxLatest, classType, nil
-}
 
 // Class is the base class.
 type Class struct {
@@ -93,7 +36,7 @@ type Class struct {
 
 // ClassInfo is the class info.
 type ClassInfo struct {
-	SID      string
+	ID      string
 	Type     string
 	Instance any
 }
--- pkg/permcode/types/policy.go
+++ pkg/permcode/types/policy.go
@@ -16,118 +16,13 @@
 
 package types
 
-import (
-	"fmt"
-	"strings"
-
-	aztext "github.com/permguard/permguard-core/pkg/extensions/text"
-)
-
-const (
-	// UUR format string: {parition}:{account}:{tenant}:{domain}:{resource}:{resource-filter}.
-	uurFormatString = "uur:%s:%s:%s:%s:%s%s"
-	// AR format string: {resource}:{action}.
-	arFormatString = "ar:%s:%s"
-	// resourceFilterSeparator is the separator for the resource filter.
-	resourceFilterSeparator = "/"
-	// KeywordPartition is the account keyword.
-	KeywordPartition = "$partition"
-	// KeywordAccount is the account keyword.
-	KeywordAccount = "$account"
-	// KeywordTenant is the tenant keyword.
-	KeywordTenant = "$tenant"
-)
-
-// UURString is the UUR wildcard string.
-type UURString aztext.WildcardString
-
-// FormatUURString formats the UUR string.
-func FormatUURString(partition, account string, tenant, domain, resource aztext.WildcardString, resourceFileter []aztext.WildcardString) UURString {
-	resFilter := ""
-	for _, f := range resourceFileter {
-		resFilter = fmt.Sprintf("%s%s%s", resFilter, resourceFilterSeparator, f)
-	}
-	return UURString(fmt.Sprintf(uurFormatString, partition, account, tenant, domain, resource, resFilter))
-}
-
-// UUR is the Universally Unique Resource.
-type UUR struct {
-	Partition      string
-	Account        string
-	Tenant         aztext.WildcardString
-	Domain         aztext.WildcardString
-	Resource       aztext.WildcardString
-	ResourceFilter []aztext.WildcardString
-}
-
-// Prase parses the UUR string.
-func (s *UURString) Prase() (*UUR, error) {
-	uurStr := string(*s)
-	parts := strings.Split(uurStr, ":")
-	if len(parts) != 6 || parts[0] != "uur" {
-		return nil, fmt.Errorf("permcode: invalid uur string %s", uurStr)
-	}
-	partition := parts[1]
-	account := parts[2]
-	tenant := parts[3]
-	domain := parts[4]
-	resParts := strings.Split(parts[5], resourceFilterSeparator)
-	resource := resParts[0]
-	resourceFilter := []aztext.WildcardString{}
-	if len(resParts) > 1 {
-		for _, filter := range resParts[1:] {
-			resourceFilter = append(resourceFilter, aztext.WildcardString(filter))
-		}
-	}
-	return &UUR{
-		Partition:      partition,
-		Account:        account,
-		Tenant:         aztext.WildcardString(tenant),
-		Domain:         aztext.WildcardString(domain),
-		Resource:       aztext.WildcardString(resource),
-		ResourceFilter: resourceFilter,
-	}, nil
-}
-
-// ARString is the AR wildcard string.
-type ARString aztext.WildcardString
-
-// FormatARString formats the AR string.
-func FormatARString(resource, action aztext.WildcardString) ARString {
-	return ARString(fmt.Sprintf(arFormatString, resource, action))
-}
-
-// Prase parses the UUR string.
-func (s *ARString) Prase() (*AR, error) {
-	arStr := string(*s)
-	parts := strings.Split(arStr, ":")
-	if len(parts) != 3 || parts[0] != "ar" {
-		return nil, fmt.Errorf(`permcode: invalid ar string '%s'`, arStr)
-	}
-	resource := parts[1]
-	action := parts[2]
-	return &AR{
-		Resource: aztext.WildcardString(resource),
-		Action:   aztext.WildcardString(action),
-	}, nil
-}
-
-// AR is the Action Resource.
-type AR struct {
-	Resource aztext.WildcardString
-	Action   aztext.WildcardString
-}
-
 // Policy is the policy.
 type Policy struct {
 	Class
-	Name     string     `json:"name"`
-	Actions  []ARString `json:"actions"`
-	Resource UURString  `json:"resource"`
 }
 
 // PolicyInfo is the policy info.
 type PolicyInfo struct {
-	SID    string
+	ID    string
 	Policy *Policy
 }
